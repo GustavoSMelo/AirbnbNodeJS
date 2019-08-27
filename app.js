96b8dee5d13947704d030222de945e2891e608d5
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-08-26 23:41:48 -0300

    Main archive for configures

---
+++ app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//Loading modules
+    const express = require('express');
+    const app = express();
+    const handlebars = require('express-handlebars');
+    const path = require('path');
+    const body_parser = require('body-parser'); 
+    const user = require('./routes/userRoutes');
+//configurations
+
+    //handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+    
+    //bodyparser
+        app.use(body_parser.json());
+        app.use(body_parser.urlencoded({extended: true}));
+
+    //routes
+        app.use('/user', user);
+
+//routes
+
+    app.get('/', (req, res) =>{
+        res.render(`${__dirname}/views/index`, {title: 'test'});
+    });
+
+    
+//other
+
+    express.static(path.join('public'));
+
+    const PORT = 9997;
+    app.listen(PORT, () =>{
+        console.log('Server running in port: http://localhost:9997/');
+    });
