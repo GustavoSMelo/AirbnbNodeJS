90618300e070997847b62c2589030987db1152f0
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-08-30 23:15:41 -0300

    creating a system of login

--- config/authentication.js
+++ config/authentication.js
@@ -3,8 +3,38 @@
 const localStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const user = require('./../models/user');
+const user = mongoose.model('user');
 
 module.exports = function(passport){
-    passport.use
+        passport.use(new localStrategy(
+            {usernameField: 'email', passwordField: 'password'}, (email, password, done) =>{
+            user.findOne({email_user: email}).then((user) =>{
+                if(!user){
+                    console.log('Email not find in our system')
+                    return done(null, false, console.log('Email not find in our system'));
+                }
+                else{
+                    bcrypt.compare(password, user.password_user, (err, validate) =>{
+                        if(validate){
+                            console.log('sucess');
+                            return done(null, user);
+                        }
+                        else{
+                            console.log('error');
+                            return done(null, false);
+                        }
+                    });
+                }
+            });
+        }));
+
+        passport.serializeUser((user, done) =>{
+            done(null, user._id);
+        });
+
+        passport.deserializeUser((id, done) =>{
+            user.findById(id, (err, user) =>{
+                done(err, user);
+            });
+        });
 };
