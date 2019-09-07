21679411db9040c7fc12e8ea747f0dbd264d8127
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-09-06 23:16:18 -0300

    add a collections for hotels

---
+++ models/hotel.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/airbnb', {useNewUrlParser: true});
+
+const hotel = mongoose.Schema({
+    name_hotel: {
+        type: String,
+        require: true
+    },
+
+    country_hotel:{
+        type: String,
+        require: true
+    },
+
+    longitude_hotel:{
+        type: Number,
+        require: true
+    },
+
+    latitude_hotel:{
+        type: Number,
+        require: true
+    },
+
+    stars_hotel: {
+        type: Number,
+        require: true
+    },
+
+    stadia_hotel: {
+        type: Number,
+        require: true
+    },
+
+    image_hotel: {
+        type: String,
+    },
+
+    createAt: {
+        type: Date,
+        require: true, 
+        default: Date.now()
+    }
+});
+
+mongoose.model('hotel', hotel);
+const h = mongoose.model('hotel');
+
+module.exports = h;
