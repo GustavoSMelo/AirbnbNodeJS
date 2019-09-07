300784a1dbd67f3e8b232b9d1d3d3ef14322dd9e
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-09-06 23:25:42 -0300

    Insert a codes for use a map and references your controls

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3p0cnlrZXJ6IiwiYSI6ImNrMDhsemd1bTA1ajQzbm51dm90bHVoMDQifQ.E492sBpjVcJx072hp0OOyg';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+const marker = new mapboxgl.Marker({
+    draggable: true
+}).setLngLat([0, 0]).addTo(map);
+
+map.addControl(new mapboxgl.NavigationControl());
+const long = window.document.querySelector('#longitudeinput');
+const lat = window.document.querySelector('#latitudeinput');
+
+const onDragEnd = () =>{
+    const LngLat = marker.getLngLat();
+    long.value = `${LngLat.lng}`;
+    lat.value = `${LngLat.lat}`;
+}
+
+marker.on('dragend', onDragEnd);
+
