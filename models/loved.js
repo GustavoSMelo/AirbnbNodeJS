df16b06fdfba971a9be2f09d246c3533fd0bf39b
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-10-02 23:25:43 -0300

    model of likes in hotels

---
+++ models/loved.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/airbnb',{useNewUrlParser: true});
+
+const love = mongoose.Schema({
+    id_hotel: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+
+    id_user:{
+        type: mongoose.Schema.Types.ObjectId
+    }, 
+
+    createAt: {
+        type: Date,
+        default: Date.now()
+    },
+
+    isLoved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+mongoose.model('loved', love);
+const loved = mongoose.model('loved');
+
+module.exports = loved;
